650c9244cab7cdb6fb9beafb4d6bd7a292c05387
Nicolas Semenas <nicolas@Nicolass-MacBook-Pro.local>
2014-08-11 03:04:19 -0300

    first commit

---
+++ Autolayout2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Autolayout2
+//
+//  Created by Nicolas Semenas on 07/08/14.
+//  Copyright (c) 2014 Nicolas Semenas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Autolayout2Tests/Autolayout2Tests.m
@@ -0,0 +1,34 @@
+//
+//  Autolayout2Tests.m
+//  Autolayout2Tests
+//
+//  Created by Nicolas Semenas on 07/08/14.
+//  Copyright (c) 2014 Nicolas Semenas. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Autolayout2Tests : XCTestCase
+
+@end
+
+@implementation Autolayout2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
